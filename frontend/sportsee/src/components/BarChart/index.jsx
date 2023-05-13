36167dce09dca115eb44d200b7b77adb944b7657
Tom Pujalte <tom.pujalte@gmail.com>
2023-05-13 16:15:16 +0200

    extracting data

--- frontend/sportsee/src/components/BarChart/index.jsx
+++ frontend/sportsee/src/components/BarChart/index.jsx
@@ -13,74 +13,16 @@ import {
 import { USER_ACTIVITY } from '../../data/mockedData'
 import "./barchart.scss"
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-]
 
+// console.log(USER_ACTIVITY[0].sessions)
 
-// const adaptUserActivity = (userActivity) =>
-//   userActivity.sessions.map((session) => {
-//     const formattedDay = new Intl.DateTimeFormat('fr', {
-//       day: 'numeric',
-//     }).format(new Date(session.day))
-
-//     return {
-//       day: +formattedDay,
-//       kilogram: session.kilogram,
-//       calories: session.calories,
-//     }
-//   })
-console.log(data[0].name)
-console.log(USER_ACTIVITY[0].sessions)
-
-function BarChartActivity() {
+function BarChartActivity({activity}) {
   return (
     <ResponsiveContainer className="activity-container" width="100%" aspect={4}>
       <BarChart
         width={500}
         height={300}
-        data={USER_ACTIVITY[0].sessions}
+        data={activity}
         margin={{
           top: 5,
           right: 30,
--- frontend/sportsee/src/pages/Home/index.jsx
+++ frontend/sportsee/src/pages/Home/index.jsx
@@ -1,7 +1,48 @@
-const { default: BarChartUI } = require("../../components/BarChart");
+import { useEffect, useState } from 'react'
+import BarChartActivity from '../../components/BarChart'
+
+// function adaptUserActivity(userActivity) {
+//   userActivity.map((session) => {
+//     const formattedDay = new Intl.DateTimeFormat('fr', {
+//       day: 'numeric',
+//     }).format(new Date(session.day))
+
+//     return {
+//       day: +formattedDay,
+//       kilogram: session.kilogram,
+//       calories: session.calories,
+//     }
+//   })
+// }
 
 function Home() {
-  return <BarChartUI />
+  const [activityData, setActivityData] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    async function fetchActivity() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:3000/user/12/activity`)
+        const { activityData } = await response.json()
+        setActivityData(activityData)
+        console.log(activityData)
+      } catch (err) {
+        console.log('===== error =====', err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchActivity()
+  }, [])
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
+
+  return <BarChartActivity activity={activityData.sessions} />
 }
 
 export default Home
